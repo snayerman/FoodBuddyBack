8aa63b4e2ac202e104bd3863f414a36b892fd255
OrangeJewc <samnayerman@gmail.com>
2018-05-06 21:22:00 -0700

    sdf

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(function(req, res, next) {
    // res.header("Access-Control-Allow-Origin", "http://localhost:5000");
-   var allowedOrigins = ['http://localhost:5000', 'https://murmuring-dusk-37367.herokuapp.com/'];
-   var origin = req.headers.origin;
+   // var allowedOrigins = ['http://localhost:5000', 'https://murmuring-dusk-37367.herokuapp.com'];
+   // var origin = req.headers.origin;
    
-   if(allowedOrigins.indexOf(origin) > -1){
-         res.setHeader('Access-Control-Allow-Origin', origin);
-   }
-
+   // if(allowedOrigins.indexOf(origin) > -1){
+   //       res.setHeader('Access-Control-Allow-Origin', origin);
+   // }
+   res.header("Access-Control-Allow-Origin", "https://murmuring-dusk-37367.herokuapp.com");
    res.header("Access-Control-Allow-Credentials", true);
    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
