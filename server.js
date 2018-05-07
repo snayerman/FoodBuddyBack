f9ed89a059773d85269e1efc64865413ec074285
OrangeJewc <samnayerman@gmail.com>
2018-05-06 21:13:06 -0700

    CORS

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "http://localhost:5000");
+   // res.header("Access-Control-Allow-Origin", "http://localhost:5000");
+   res.header("Access-Control-Allow-Origin", "*");
    res.header("Access-Control-Allow-Credentials", true);
    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
